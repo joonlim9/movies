fc91ad9ccc69f537d2ecec905bcd13d0dea56a04
joonlim9 <joonlim@umich.edu>
2022-03-09 18:54:01 -0500

    template inheritance

--- index.py
+++ index.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    data = "This is My Movie Search Website"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
